26f737da7eb27ad1a7dfd137583046cf602d8bf1
zhanghao <839329312@qq.com>
2015-03-19 13:54:43 +0800

    程序1

---
+++ zhang.c
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void main()
+{
+ 
+  ###############################
+  ######                   ######
+  ######     string        ######
+  ######                   ######
+  ###############################
+
+} 
